7c2c875b57c807dfc99b1bd3c0d503429fd024d9
Ethan Banovic <st188321@stud.uni-stuttgart.de>
2024-11-10 01:59:12 +0100

    First Backend Work

---
+++ backend/backend/src/main/java/de/team5/sopra/backend/models/Day.java
@@ -0,0 +1,10 @@
+package de.team5.sopra.backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Day {
+    @Id
+    private long id;
+}
---
+++ backend/backend/src/main/java/de/team5/sopra/backend/models/Week.java
@@ -0,0 +1,10 @@
+package de.team5.sopra.backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Week {
+    @Id
+    private long id;
+}
